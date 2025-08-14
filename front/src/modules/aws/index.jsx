5a099b6ea278d5cf7b8777a4a2cdb79f914334a4
jhonmike <jhon.msdev@gmail.com>
2025-08-14 01:21:37 -0300

    fix(aws): implement proper routing and menu structure
    - Fix AWS module to use generic ContentWithMenu with paramName='key'
    - Correct menu link generation to use account key instead of name
    - Fix page element structure from component to element prop
    - Enable proper URL parameter substitution for AWS routes
    - Ensure AWS module follows same patterns as Kubernetes module

--- front/src/modules/aws/index.jsx
+++ front/src/modules/aws/index.jsx
@@ -1,15 +1,15 @@
 import { CloudOutlined } from "@ant-design/icons"
 import InstancePage from "./InstancePage"
 import { getAccounts } from "./accountResource"
-import ContentWithMenu from "../kubernetes/ContentWithMenu"
+import ContentWithMenu from "../../components/ContentWithMenu"
 
 const AwsModule = async () => {
   const { data } = await getAccounts()
-  const menus = data.map(({ name }) => ({
+  const menus = data.map(({ name, key }) => ({
     label: name,
     icon: <CloudOutlined />,
-    key: `aws-${name}`,
-    link: `/aws/${name}`,
+    key: `aws-${key}`,
+    link: `/aws/${key}`,
   }))
 
   const pages = [
@@ -17,7 +17,7 @@ const AwsModule = async () => {
       name: "EC2 Instances",
       path: "/aws/:key/ec2",
       menu: true,
-      component: InstancePage,
+      element: <InstancePage />,
     },
   ]
 
@@ -27,7 +27,7 @@ const AwsModule = async () => {
       {
         key: "aws",
         path: "/aws/:key/*",
-        element: <ContentWithMenu pages={pages} />,
+        element: <ContentWithMenu pages={pages} paramName="key" />,
       },
     ],
   }
